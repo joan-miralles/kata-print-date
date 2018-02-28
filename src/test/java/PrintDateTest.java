aeabef9353f239ce3fc7f377171d9d3eb9ce51d9
Joan Miralles <jmiralles@brujula.es>
2018-02-28 17:04:03 +0100

    Add mock DateProvider to return always the same date

--- src/test/java/PrintDateTest.java
+++ src/test/java/PrintDateTest.java
@@ -1,11 +1,14 @@
 import org.junit.Test;
 
+import java.time.Instant;
 import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class PrintDateTest {
     private Printer printer;
@@ -25,7 +28,8 @@ public class PrintDateTest {
 	@Test
     public void printDate_withMockito() {
         printer = spy(Printer.class);
-        dateProvider = new DateProvider();
+        dateProvider = mock(DateProvider.class);
+        when(dateProvider.currentDate()).thenReturn(Date.from(Instant.parse("2018-02-28T17:02:30.00Z")));
         PrintDate printDate = new PrintDate(printer, dateProvider);
         printDate.printCurrentDate();
         verify(printer).print(any());
